7ce981e53f2c4e687c3829c5e72a47a5603220b0
chenjun <425781439@qq.com>
2021-07-19 17:45:01 +0800

    text center

--- app/src/main/java/com/example/cjseek/ui/category/Category.kt
+++ app/src/main/java/com/example/cjseek/ui/category/Category.kt
@@ -1,12 +1,20 @@
 package com.example.cjseek.ui.category
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CategoryScreen() {
@@ -16,13 +24,25 @@ fun CategoryScreen() {
 @Composable
 fun CategoryContent(onClick: () -> Unit) {
     Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Nav Compose", modifier = Modifier.clickable { onClick() })
+        Text(
+            text = "Nav Compose",
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(10.dp))
+                .background(
+                    Color.Cyan
+                )
+                .padding(10.dp)
+                .clickable {
+                    onClick()
+                }
+                .align(Alignment.Center)
+        )
     }
 }
 
 @Preview
 @Composable
-fun PreviewCategoryContent(){
+fun PreviewCategoryContent() {
     CategoryContent {
 
     }
