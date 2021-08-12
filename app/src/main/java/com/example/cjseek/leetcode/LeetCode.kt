15d154658382e4d7d1d14c46de58e5882fc1bf08
chenjun <425781439@qq.com>
2021-08-12 16:41:18 +0800

    feat: 算法

---
+++ app/src/main/java/com/example/cjseek/leetcode/LeetCode.kt
@@ -0,0 +1,36 @@
+package com.example.cjseek.leetcode
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val indexs = IntArray(2)
+
+
+        return indexs
+    }
+
+    /**
+     * 描述
+    输入一个链表，反转链表后，输出新链表的表头。
+    示例1
+    输入：{1,2,3}
+    返回值：{3,2,1}
+     */
+    fun reverseList(head: ListNode?): ListNode? {
+
+
+        return null
+    }
+}
+
+data class ListNode(val value: Int) {
+    var next: ListNode? = null
+}
---
+++ app/src/test/java/com/example/cjseek/SolutionTest.kt
@@ -0,0 +1,14 @@
+package com.example.cjseek
+
+import org.junit.Test
+
+/**
+ * @author chenjun
+ * 创建日期：2021/8/12
+ */
+class SolutionTest {
+    @Test
+    fun twoSum_isCorrect() {
+
+    }
+}
